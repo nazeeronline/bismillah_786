f84818089fb83d87769c300932ace193cb0859dd
Nazeer Hussain <nazeeronline@gmail.com>
2012-08-01 16:48:10 +0530

    Bismillah

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bismillah::Application.config.secret_token = 'cca7bca0d2cbf2b5f8903095c82d1352c1c86929d2e483c636112489c2363e8ff0d9b5079ac4624885ba72d55eadce0da418a7896c2e2df7efd3d2ff816f9c9b'
